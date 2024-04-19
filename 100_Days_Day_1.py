de8d3bd2a32813e5eff44081fc0bf845427d3b80
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-04-19 11:13:36 -0500

    Add files via upload
    Just getting started...

---
+++ 100_Days_Day_1.py
@@ -0,0 +1,6 @@
+# Band Name Generator #
+
+print("Welcome to the Band Name Generator.  Let's think of a name to call your band.")
+print("First question: What's the name of the city you grew up in?")
+print("Second question: What's your pet's name?")
+print("So essentially, a possible name for your band could be \"\".")
